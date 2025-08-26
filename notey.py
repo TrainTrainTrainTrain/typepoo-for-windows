117390f3eaeb400de5678be1e15f5df0df12375a
TrainTrainTrainTrain <mark.hughes.fn@gmail.com>
2025-08-26 18:03:36 -0400

    Add files via upload

---
+++ notey.py
@@ -0,0 +1,50 @@
+import subprocess
+import sys
+
+package_name = "pynput" # Replace with the package you want to install
+
+
+try:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+except subprocess.CalledProcessError as e:
+            print(f"Error installing {package_name}: {e}")
+
+
+
+
+from pynput import keyboard
+import time
+
+# Create a keyboard controller object
+keyboard_controller = keyboard.Controller()
+
+# windows key + R
+keyboard_controller.press(keyboard.Key.cmd)   
+keyboard_controller.press('r')               
+time.sleep(0.1)                               
+keyboard_controller.release('r')              
+keyboard_controller.release(keyboard.Key.cmd) 
+
+#deleats whats in it
+
+keyboard_controller.press(keyboard.Key.ctrl_l)
+keyboard_controller.press(keyboard.Key.backspace)
+time.sleep(0.1)
+keyboard_controller.release(keyboard.Key.backspace)
+keyboard_controller.release(keyboard.Key.ctrl_l)
+
+#opens notepad
+keyboard_controller.type("notepad")
+keyboard_controller.press(keyboard.Key.enter) 
+
+#makes new notepad
+keyboard_controller.press(keyboard.Key.ctrl_l)
+keyboard_controller.press('t')
+time.sleep(0.1)
+keyboard_controller.release(keyboard.Key.backspace)
+keyboard_controller.release('t')
+time.sleep(1.0)
+
+#types "poop"
+keyboard_controller.type("POOP!!!!")
+
